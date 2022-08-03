f5cc4873c7d4a3f79492eeb5cc30b4615d00750c
Patrick Jarvis <patrickj.jarvis@gmail.com>
2022-08-03 23:28:17 +0100

    commit

--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import './header.css';
+import catcargo from '/Users/jarvp/Github /paddypedalpower/src/img/cargobike-cat.png';
 
 const header = () => {
   return (
-    <div>header</div>
+    <div className='header section_padding' id='home'>
+      <div className='header-content'>
+        <h1 className='gradient_text'>Everesting with my shopping, tackling ty tumblr.</h1>
+        
+          <p>Where I live is too hilly to ride my bike.
+             It takes to long to cycle places.
+             I'm not fit enough to ride a bike.
+             It's too dangerous to ride on the roads.
+             Enough excuses, lets show what is possible.
+          </p>
+          <div className='header-content_input'>
+          <input type='email' placeholder='Your email address'></input>
+        <button> Learn more</button>
+        <div className='header-image'>
+            <img src={catcargo} alt="cat in cargo bike"/>
+           </div>
+        </div>
+        <div className='header-content_people'>
+           </div>
+
+           
+      </div>
+      </div>
   )
 }
 
