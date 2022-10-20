34699703bc3fc601c07869957deeb814e7343088
Patrick Jarvis <patrickj.jarvis@gmail.com>
2022-10-20 16:42:21 +0100

    commit

--- src/components/brand/brand.jsx
+++ src/components/brand/brand.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './brand.css';
+import { google, slack, atlassian, dropbox, shopify} from './imports';
 
 const brand = () => {
   return (
-    <div>brand</div>
+    <div className='brand section_padding'>
+    <div>
+      <img src={google} alt="google"></img>
+      <img src={slack} alt="slack"></img>
+      <img src={atlassian} alt="atlassian"></img>
+      <img src={dropbox} alt="dropbox"></img>
+      <img src={shopify} alt="shopify"></img>
+    </div>
+    </div>
   )
 }
 
